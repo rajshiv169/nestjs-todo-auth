e6c89426d98eb4a5a5ec7d7b80bd13bd12025411
Shiv Raj <20968420+rajshiv169@users.noreply.github.com>
2025-03-26 20:43:30 +0530

    Add Swagger decorators to CreateTodoDto

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,10 +1,20 @@
 import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTodoDto {
+  @ApiProperty({
+    example: 'Buy groceries',
+    description: 'The title of the todo',
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    example: 'Milk, eggs, bread',
+    description: 'Optional description of the todo',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   description?: string;
