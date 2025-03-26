0123010f60e4f0a558f651807dc8d072404ad0d9
Shiv Raj <20968420+rajshiv169@users.noreply.github.com>
2025-03-26 18:57:56 +0530

    Add register DTO

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, MinLength, IsString, Matches } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+  })
+  password: string;
+}
