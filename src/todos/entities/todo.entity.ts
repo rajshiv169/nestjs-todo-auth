370e6cb1ae79d2b464a3cc008128aaa0340fb3c2
Shiv Raj <20968420+rajshiv169@users.noreply.github.com>
2025-03-26 18:57:24 +0530

    Add todo entity

---
+++ src/todos/entities/todo.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ default: false })
+  completed: boolean;
+
+  @Column({ nullable: true, type: 'text' })
+  description: string;
+
+  @ManyToOne(() => User, (user) => user.todos, { onDelete: 'CASCADE' })
+  user: User;
+
+  @Column()
+  userId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
