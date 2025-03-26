57cb07e6224c4894678198273da089c8b5a14d61
Shiv Raj <20968420+rajshiv169@users.noreply.github.com>
2025-03-26 20:38:20 +0530

    Add create todo DTO

---
+++ src/todos/dto/create-todo.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
